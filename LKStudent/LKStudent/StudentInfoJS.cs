0acaf305c5e95f638fab328be3943bb6205581c7
ultimatum424 <ultimatum424@yandex.ru>
2016-08-30 17:11:20 +0300

    Страница с информацией о студенте

---
+++ LKStudent/LKStudent/StudentInfoJS.cs
@@ -0,0 +1,26 @@
+﻿namespace LKStudent
+{
+    public class StudentInfoJS
+    {
+        public string Fio { get; set; }
+        public string StudentNumber { get; set; }
+        public string AcademicState { get; set; }
+        public string GroupName { get; set; }
+
+        public Oop Oop;
+        public EditGroup EditGroup;
+    }
+    public class Oop
+    {
+        public string Faculty { get; set; }
+        public string FullStdName { get; set; }
+        public string FullSpzName { get; set; }
+    }
+
+    public class EditGroup
+    {
+        public string Course { get; set; }
+    }
+
+
+}
