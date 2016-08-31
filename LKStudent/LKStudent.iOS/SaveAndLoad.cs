ccdaeda8374428ead6031606f92188e4bcd62379
ultimatum424 <ultimatum424@yandex.ru>
2016-08-31 18:59:00 +0300

    Реализованна возмжность сохранения файлов IOS версии

---
+++ LKStudent/LKStudent.iOS/SaveAndLoad.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LKStudent.iOS;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SaveAndLoad.SaveAndLoad))]
+
+namespace LKStudent.iOS
+{
+
+
+
+    public class SaveAndLoad : ISaveAndLoad
+    {
+        public void SaveText(string filename, string text)
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var filePath = Path.Combine(documentsPath, filename);
+            System.IO.File.WriteAllText(filePath, text);
+        }
+
+        public string LoadText(string filename)
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var filePath = Path.Combine(documentsPath, filename);
+            return System.IO.File.ReadAllText(filePath);
+        }
+    }
+}
