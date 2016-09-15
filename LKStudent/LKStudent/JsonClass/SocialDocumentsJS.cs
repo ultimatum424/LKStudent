05d242a984261f8f5c2c112cf904b63bbe570093
ultimatum424 <ultimatum424@yandex.ru>
2016-09-15 18:17:14 +0300

    Добавлена странциа с социальными документами

---
+++ LKStudent/LKStudent/JsonClass/SocialDocumentsJS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LKStudent
+{
+    public class SocialDocumentsJS
+    {
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public DateTime DateRequest { get; set; }
+        public string Category { get; set; }
+        public string GrantPeriod { get; set; }
+        
+    }
+}
---
+++ LKStudent/LKStudent/Pages/SocialDocumentsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace LKStudent.Pages
+{
+    public partial class SocialDocumentsPage : ContentPage
+    {
+        GetJsToUrl jsToUrl;
+        string url = "https://test-lks.volgatech.net/Grants/GetSocialRequestsJSON";
+        private string name = "SocialDocumentsJSON.json";
+        List<SocialDocumentsJS> socialDocumentsList;
+
+        public SocialDocumentsPage()
+        {
+            jsToUrl = new GetJsToUrl(url, name);
+            jsToUrl.LoadData(url, name);
+            this.BindingContext = jsToUrl;
+            string sToken = DependencyService.Get<ISaveAndLoad>().LoadText(name);
+            socialDocumentsList = JsonConvert.DeserializeObject<List<SocialDocumentsJS>>(sToken);
+            InitializeComponent();
+            SocialDocuments.ItemsSource = socialDocumentsList;
+        }
+    }
+}
